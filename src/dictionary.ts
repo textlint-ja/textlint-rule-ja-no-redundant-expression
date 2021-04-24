ee9a49db6b73023966104a3513dafa72c06ceb8d
azu <azuciao@gmail.com>
2021-04-24 22:30:05 +0900

    style: apply prettier

--- src/dictionary.ts
+++ src/dictionary.ts
@@ -7,9 +7,8 @@ const punctuations = ["、", "､", "，", ","];
  */
 export const ExpectedType = {
     // Expectedへの変換自体を取りやめる
-    "STOP_REPLACE": "STOP_REPLACE"
+    STOP_REPLACE: "STOP_REPLACE"
 };
-
 
 export type ExpectedTokenAdditional = {
     _skippable?: boolean;
@@ -18,47 +17,45 @@ export type ExpectedTokenAdditional = {
 // ExpectedToken is based on Token
 // But support array of each properties
 export type ExpectedToken = {
-        // 辞書内での単語ID
-        word_id?: number | number[];
-        // 単語タイプ(辞書に登録されている単語ならKNOWN; 未知語ならUNKNOWN)
-        word_type?: "KNOWN" | "UNKNOWN";
-        // 表層形
-        surface_form?: string | string[];
-        // 品詞
-        pos?: string | string[];
-        // 品詞細分類1
-        pos_detail_1?: string | string[];
-        // 品詞細分類2
-        pos_detail_2?: string | string[];
-        // 品詞細分類3
-        pos_detail_3?: string | string[];
-        // 活用型
-        conjugated_type?: string | string[];
-        // 活用形
-        conjugated_form?: string | string[];
-        // 基本形
-        basic_form?: string | string[];
-        // 読み
-        reading?: string | string[];
-        // 発音
-        pronunciation?: string | string[];
-        // 単語の開始位置
-        word_position?: number | number[];
-    }
-    & ExpectedTokenAdditional
-    & {
-    [index: string]: any;
-};
+    // 辞書内での単語ID
+    word_id?: number | number[];
+    // 単語タイプ(辞書に登録されている単語ならKNOWN; 未知語ならUNKNOWN)
+    word_type?: "KNOWN" | "UNKNOWN";
+    // 表層形
+    surface_form?: string | string[];
+    // 品詞
+    pos?: string | string[];
+    // 品詞細分類1
+    pos_detail_1?: string | string[];
+    // 品詞細分類2
+    pos_detail_2?: string | string[];
+    // 品詞細分類3
+    pos_detail_3?: string | string[];
+    // 活用型
+    conjugated_type?: string | string[];
+    // 活用形
+    conjugated_form?: string | string[];
+    // 基本形
+    basic_form?: string | string[];
+    // 読み
+    reading?: string | string[];
+    // 発音
+    pronunciation?: string | string[];
+    // 単語の開始位置
+    word_position?: number | number[];
+} & ExpectedTokenAdditional & {
+        [index: string]: any;
+    };
 export type ExpectedDictionary = {
     id: string;
-    disabled: boolean
+    disabled: boolean;
     allows: string[];
-    message: string
+    message: string;
     url: string;
     expected?: string;
     description?: string;
-    tokens: ExpectedToken[]
-}
+    tokens: ExpectedToken[];
+};
 
 export const Dictionary: ExpectedDictionary[] = [
     {
@@ -307,13 +304,13 @@ export const Dictionary: ExpectedDictionary[] = [
                 basic_form: "いる"
             },
             {
-                "surface_form": "ます",
-                "pos": "助動詞",
-                "conjugated_type": "特殊・マス",
-                "conjugated_form": "基本形",
-                "basic_form": "ます",
-                "reading": "マス",
-                "pronunciation": "マス",
+                surface_form: "ます",
+                pos: "助動詞",
+                conjugated_type: "特殊・マス",
+                conjugated_form: "基本形",
+                basic_form: "ます",
+                reading: "マス",
+                pronunciation: "マス",
                 _skippable: true
             }
         ]
--- test/dictionary-test.ts
+++ test/dictionary-test.ts
@@ -4,21 +4,21 @@ import assert from "assert";
 
 import { Dictionary } from "../src/dictionary";
 
-describe("dictionary", function() {
+describe("dictionary", function () {
     it("should not have duplicated id", () => {
-        Dictionary.forEach(item => {
+        Dictionary.forEach((item) => {
             assert.strictEqual(typeof item.id, "string", "should have id property");
-            const sameIdItems = Dictionary.filter(target => target.id === item.id);
+            const sameIdItems = Dictionary.filter((target) => target.id === item.id);
             assert.ok(sameIdItems.length === 1, "should not have duplicated id item");
         });
     });
     it("should have disabled default value", () => {
-        Dictionary.forEach(item => {
+        Dictionary.forEach((item) => {
             assert.strictEqual(typeof item.disabled, "boolean", `${item} should have disabled property`);
         });
     });
     it("should have allows default value", () => {
-        Dictionary.forEach(item => {
+        Dictionary.forEach((item) => {
             assert.ok(Array.isArray(item.allows), `${item}: should have disabled property`);
         });
     });
